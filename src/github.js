356f770cbbb640c7d7c03b7ae3651d062143e4d4
Will Ashworth <wmashworth@hotmail.com>
2018-05-20 21:38:36 +0100

    Getting there...

--- src/github.js
+++ src/github.js
@@ -2,6 +2,8 @@ import forEach from "lodash/forEach";
 import forOwn from "lodash/forOwn";
 import map from "lodash/map";
 import showdown from "showdown";
+import find from "lodash/find";
+import replace from "lodash/replace";
 
 import GitHubInteract from "./api/github/interact";
 
@@ -76,8 +78,10 @@ const getImgTag = (fileContent, imgOpenTagIndex, imgCloseTagIndex) => {
 };
 
 const getImgSrc = (imgTag) => {
+	console.log("\n===== getImgSrc =====");
 	const imgSrcOpenTagIndex = (imgTag.indexOf("src=") + 5);
 	const imgSrc = imgTag.substring(imgSrcOpenTagIndex, imgTag.indexOf('"', imgSrcOpenTagIndex));
+	console.log(`::: imgSrc: ${imgSrc}`);
 
 	return removeImgSrcRelativity(imgSrc);
 };
@@ -92,10 +96,21 @@ const getConvertedMdFiles = (mdFiles) => {
 };
 
 const removeImgSrcRelativity = (imgSrc) => {
+	console.log("\n===== removeImgSrcRelativity =====");
+	console.log(`::: contain?: ${imgSrc.indexOf("..") > -1}`);
 	if (imgSrc.indexOf("..") > -1) {
-		imgSrc.replace(/../g, "");
+		imgSrc = replace(imgSrc, /../g, "");
 	}
+	console.log(`::: imgSrc: ${imgSrc}`);
 	return imgSrc;
+};
+
+const getMatchingImageFile = (imgFiles, imgSrc) => {
+	return find(imgFiles, (imgFile) => {
+		console.log(`::: imgFile path: ${imgFile.path}`);
+		console.log(`::: imgSrc: ${imgSrc}`);
+		return (imgFile.path.toLowerCase().indexOf(imgSrc.toLowerCase()) > -1);
+	});
 };
 
 async function getKnowledgeFiles(knowledgeType, knowledge) {
@@ -109,7 +124,11 @@ async function getKnowledgeFiles(knowledgeType, knowledge) {
 	let imgCloseTagIndex = 0;
 	let imgTag = "";
 	let imgSrc;
+	let imgSrcAttrIndex = "";
 	let tagBeforeImgSrc = "";
+	let tagAfterImgSrc = "";
+	let newImgTag = "";
+	let matchingImgFile;
 
 	// imgOpenTagIndex = index of next image open tag
 	// substr 2nd param is length of tag
@@ -119,13 +138,22 @@ async function getKnowledgeFiles(knowledgeType, knowledge) {
 			imgTag = getImgTag(mdFile.content, imgOpenTagIndex, imgCloseTagIndex);
 			imgSrc = getImgSrc(imgTag);
 
-			forEach(imgFiles, (imgFile) => {
-				if (imgFile.path.toLowerCase().indexOf(imgSrc.toLowerCase()) > -1) {
-					imgSrc = imgFile.content;
-				}
-			});
-			tagBeforeImgSrc = (imgTag.indexOf("src=") + 4);
-			tagBeforeImgSrc += imgSrc;
+			console.log("::: not set :::\n");
+			matchingImgFile = getMatchingImageFile(imgFiles, imgSrc);
+			console.log("\n::: set :::");
+
+			imgSrc = matchingImgFile.content;
+
+			console.log("::: setting new image tag :::");
+			console.log(`::: imgTag: ${imgTag}`);
+			imgSrcAttrIndex = (imgTag.indexOf("src=") + 5);
+			tagBeforeImgSrc = imgTag.substring(0, imgSrcAttrIndex);
+			console.log(`::: tagBeforeImgSrc: ${tagBeforeImgSrc}`);
+			tagAfterImgSrc = imgTag.substring((imgTag.indexOf('"', imgSrcAttrIndex), imgTag.length));
+			console.log(`::: tagAfterImgSrc: ${tagAfterImgSrc}`);
+			console.log(`::: imgSrc: ${imgSrc}`);
+			newImgTag = (tagBeforeImgSrc + imgSrc + tagAfterImgSrc);
+			console.log(`::: newImgTag: ${newImgTag}`);
 			mdFile.content.replace(imgTag);
 		}
 	});
